08741fcd50313d046818a667ff66c9a0d03e81e4
Myat Min Aung <iridiamthegreat@gmail.com>
2023-02-18 10:36:53 +0630

    refactor: login hook

---
+++ src/pages/Login/Hook.js
@@ -0,0 +1,76 @@
+import { useContext, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import axios from 'axios';
+
+import { Context } from 'context/user-context';
+import { LoginSchema } from 'validations/index';
+import { baseUrl } from 'utils/baseUrl';
+
+export const useLogin = () => {
+
+    const navigate = useNavigate();
+
+    const { search } = useLocation();
+    const redirectUrl = new URLSearchParams(search).get('redirect');
+    const redirect = redirectUrl ? redirectUrl : '/';
+
+    const { state , dispatch: authDispatch } = useContext(Context);
+    const { userInfo } = state;
+ 
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [navigate, userInfo, redirect]);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [unauthorized, setUnauthorized] = useState(false); 
+
+    const handleShowPw = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const onSubmit = async (values) => { 
+        setIsLoading(true);
+        try {
+            const { data } = await axios.post(
+                `${baseUrl}/server/auth/login`, values
+            );
+
+            authDispatch({
+                type: "REQUEST_LOGIN",
+                payload: data.user
+            });
+            
+            if (data.success === true) {
+                localStorage.setItem('userInfo', JSON.stringify(data.user));
+                localStorage.setItem('accessToken', data.token)
+                navigate(redirect || '/');  
+            }
+
+        } catch (error) {
+            setUnauthorized(true)
+            setTimeout(() => {
+                setUnauthorized(false);
+            }, 5000)
+            setIsLoading(false);
+        }
+    };
+
+    const { values, handleSubmit, handleChange, touched, errors } = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validateOnBlur: true,
+        onSubmit,
+        validationSchema: LoginSchema
+    });
+
+    return {
+        isLoading, showPassword, unauthorized, handleShowPw,
+        values, handleSubmit, handleChange, touched, errors
+    }
+};
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,71 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { BiShow, BiHide } from 'react-icons/bi';
-import { useFormik } from 'formik';
-import axios from 'axios';
 
-import { Context } from 'context/user-context';
-import { LoginSchema } from 'validations/index';
 import { WebTitle, ErrorField, SubTitle } from 'components';
-import { baseUrl } from 'utils/baseUrl';
+import { useLogin } from './Hook';
 
 const Login = () => {
 
-    const navigate = useNavigate();
-
-    const { search } = useLocation();
-    const redirectUrl = new URLSearchParams(search).get('redirect');
-    const redirect = redirectUrl ? redirectUrl : '/';
-
-    const { state , dispatch: authDispatch } = useContext(Context);
-    const { userInfo } = state;
- 
-    useEffect(() => {
-        if (userInfo) {
-            navigate(redirect);
-        }
-    }, [navigate, userInfo, redirect]);
-
-    const [isLoading, setIsLoading] = useState(false);
-    const [showPassword, setShowPassword] = useState(false);
-    const [unauthorized, setUnauthorized] = useState(false); 
-
-    const onSubmit = async (values) => { 
-        setIsLoading(true);
-        try {
-            const { data } = await axios.post(
-                `${baseUrl}/server/auth/login`, values
-            );
-
-            authDispatch({
-                type: "REQUEST_LOGIN",
-                payload: data.user
-            });
-            
-            if (data.success === true) {
-                localStorage.setItem('userInfo', JSON.stringify(data.user));
-                localStorage.setItem('accessToken', data.token)
-                navigate(redirect || '/');  
-            }
-
-        } catch (error) {
-            setUnauthorized(true)
-            setTimeout(() => {
-                setUnauthorized(false);
-            }, 5000)
-            setIsLoading(false);
-        }
-    };
-
-    const { values, handleSubmit, handleChange, touched, errors } = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
-        },
-        validateOnBlur: true,
-        onSubmit,
-        validationSchema: LoginSchema
-    });
+    const {
+        isLoading, showPassword, unauthorized, handleShowPw,
+        values, handleSubmit, handleChange, touched, errors
+    } = useLogin();
 
     return (
         <section className="px-3 py-6 lg:px-6 mb-10">
@@ -110,7 +55,7 @@ const Login = () => {
                             onChange={handleChange}
                             className="w-[100%] text-sm placeholder:text-sm focus:outline-none"
                         />
-                        <div onClick={() => setShowPassword(!showPassword)} className="cursor-pointer">
+                        <div onClick={handleShowPw} className="cursor-pointer">
                             {!showPassword ? (
                                 <BiHide size={23} color="#0F2027" />
                             ) : (
