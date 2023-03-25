7277c4b08c1bd06242d3d03318e5bfd5839d070d
Myat Min Aung <iridiamthegreat@gmail.com>
2023-03-25 22:47:24 +0630

    fix: accessToken fnc

--- src/pages/OrderHistory/Hook.js
+++ src/pages/OrderHistory/Hook.js
@@ -2,11 +2,12 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 import { baseUrl } from "utils/baseUrl";
+import { GET_ACCESS_TOKEN } from "utils/accessToken";
 
 const useOrderHistory = () => {
     const { isLoading, error, data } = useQuery("orderHistory", async () => {
         const { data } = await axios.get(`${baseUrl}/server/orders/client`, {
-            headers: { authorization: `Bearer ${localStorage.getItem("accessToken")}` },
+            headers: { authorization: `Bearer ${GET_ACCESS_TOKEN}` },
         });
         return data;
     });
--- src/utils/accessToken.js
+++ src/utils/accessToken.js
@@ -4,3 +4,9 @@ export const accessToken = localStorage.getItem("accessToken");
 // export const getAccessToken = () => {
 //     JSON.parse(localStorage.getItem("accessToken"))
 // };
+
+const getAccessToken = (key) => {
+    return localStorage.getItem(key);
+};
+
+export const GET_ACCESS_TOKEN = getAccessToken("accessToken");
