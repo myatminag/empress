79df28bc6754af5ac34a4a9a432adbb3a8496c61
Myat Min Aung <iridiamthegreat@gmail.com>
2023-03-28 21:33:40 +0630

    refactor: register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,75 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { BiShow, BiHide } from 'react-icons/bi';
-import { useFormik } from 'formik';
-import axios from 'axios';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Context } from 'context/user-context'; 
-import { SignupSchema } from 'validations';
+import PwShowIcon from 'components/icons/PwShowIcon';
+import PwHideIcon from 'components/icons/PwHideIcon';
 import { WebTitle, ErrorField, SubTitle } from 'components';
-import { baseUrl } from 'utils/baseUrl';
+import useRegister from './hook';
 
 const Login = () => {
 
-    // after register, let the user to redirect current url
-    const navigate = useNavigate();
-
-    const { search } = useLocation();
-    const redirectUrl = new URLSearchParams(search).get('redirect'); 
-    const redirect = redirectUrl ? redirectUrl : '/';
-
-    const { state, dispatch: authDispatch } = useContext(Context);
-    const { userInfo } = state;
-
-    useEffect(() => {
-        if (userInfo) {
-            navigate(redirect);
-        }
-    }, [userInfo, navigate, redirect]);
-
-    const [isLoading, setIsLoading] = useState(false);
-    const [showPassword, setShowPassword] = useState(false);
-    const [showCPassword, setShowCPassword] = useState(false);
-
-    const onSubmit = async (values) => {
-        setIsLoading(true);
-        try { 
-            const { data } = await axios.post(
-                `${baseUrl}/server/auth/register`, values
-            );
-
-            authDispatch({
-                type: "REQUEST_LOGIN", 
-                payload: data.user
-            });
-
-            if (data.success === true) {
-                localStorage.setItem('userInfo', JSON.stringify(data.user));
-                localStorage.setItem('accessToken', data.token)
-                navigate(redirect || '/');  
-            }
-        } catch (error) {
-            navigate('*');
-        }
-    };
-
-    const { values, handleSubmit, handleChange, touched, errors, isSubmitting } = useFormik({ 
-        initialValues: {
-            username: '',
-            email: '',
-            password: '',
-            cpassword: ''
-        },
-        validateOnBlur: true,
-        onSubmit,
-        validationSchema: SignupSchema
-    });
-    
+    const {
+        isLoading,
+        showPassword, setShowPassword,
+        showCPassword, setShowCPassword,
+        values,
+        handleSubmit,
+        handleChange,
+        touched,
+        errors,
+        isSubmitting
+    } = useRegister();
+  
     return (
         <section className="px-3 py-6 lg:px-6 mb-10">  
             <WebTitle title={"Register"} />
             <SubTitle name={"Register"} />
             <form onSubmit={handleSubmit} className="md:w-[450px] md:mx-auto">
+                {/* ----- username ----- */}
                 <div className="mb-4">
                     <label htmlFor="username" className="block mb-2">
                         Username
@@ -81,9 +37,10 @@ const Login = () => {
                         placeholder="Please enter username"
                         value={values.username}
                         onChange={handleChange}
-                        className="w-[100%] px-4 py-2 rounded-md border text-sm placeholder:text-sm focus:outline-none"
+                        className="input-form"
                     />
                 </div>
+                {/* ----- email ----- */}
                 <div className="mb-4">
                     <label htmlFor="email" className="block mb-2">
                         Email Address
@@ -95,9 +52,10 @@ const Login = () => {
                         placeholder="Please enter email address"
                         value={values.email}
                         onChange={handleChange}
-                        className="w-[100%] px-4 py-2 rounded-md border text-sm placeholder:text-sm focus:outline-none"
+                        className="input-form"
                     />
                 </div>
+                {/* ----- password ----- */}
                 <div className="mb-4">
                     <label className="block mb-2">
                         Password
@@ -114,13 +72,14 @@ const Login = () => {
                         />
                         <div onClick={() => setShowPassword(!showPassword)} className="cursor-pointer">
                             {!showPassword ? (
-                                <BiHide size={23} color="#0F2027" />
+                                <PwHideIcon />
                             ) : (
-                                <BiShow size={23} color="#0F2027" />
+                                <PwShowIcon />
                             )}
                         </div>
                     </div>
                 </div>
+                {/* ----- confirm password ----- */}
                 <div className="mb-8">
                     <label className="block mb-2">
                         Confirm Password
@@ -137,9 +96,9 @@ const Login = () => {
                         />
                         <div onClick={() => setShowCPassword(!showCPassword)} className="cursor-pointer">
                             {!showCPassword ? (
-                                <BiHide size={23} color="#0F2027" />
+                                <PwHideIcon />
                             ) : (
-                                <BiShow size={23} color="#0F2027" />
+                                <PwShowIcon />
                             )}
                         </div>
                     </div>
@@ -147,7 +106,7 @@ const Login = () => {
                 <button 
                     type="submit"
                     disabled={isSubmitting}
-                    className="w-[100%] px-4 py-2 mb-6 text-sm text-white tracking-wider bg-primaryDark border border-primaryDark hover:text-primaryDark hover:bg-white transition duration-200"
+                    className="default-btn"
                 >
                     {isLoading ? "Loading..." : "Register"}
                 </button>
